4be2e028fe1c90d92cf35e3605cf61897ec794ad
2015E8007361074 <ieeflsyu@outlook.com>
2016-03-19 01:13:00 +0800

    第一个爬虫

--- note.py
+++ note.py
@@ -5,4 +5,11 @@ print 'Hello', lan
 '''
 网络爬虫是一种按照一定的规则，自动的抓取万维网信息的程序或者脚本。
 '''
-
+#根据网站：http://cuiqingcai.com/927.html分享的教程内容，学习Python爬虫需要学习一下几点
+'''
+1.Python基础知识
+2.Python中urllib和urllib2库的用法
+3.Python正则表达式
+4.Python爬虫框架Scrapy
+6.Python爬虫更高级的功能
+'''
