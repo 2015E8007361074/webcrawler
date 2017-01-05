4fa27f67440aa4eb184cb9d1cc61b0924eac0b09
wayne <ieeflsyu@outlook.com>
2017-01-05 23:25:28 +0800

    我的python爬虫学习笔记

---
+++ mryu/get_links.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+pages = set()
+
+
+def get_links(page_url):
+    global pages
+    html = urlopen("http://mryu.top/")
+    bs_obj = BeautifulSoup(html)
+    for link in bs_obj.findAll("a"):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                # This is a new page
+                new_page = link.attrs['href']
+                print(new_page)
+                pages.add(new_page)
+                get_links(new_page)
+get_links("")
